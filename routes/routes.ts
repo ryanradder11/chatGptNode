3850c9042f0a76de50015abb1d56fd7315e8797a
ryanradder <ryan.radder@headfirst.nl>
2023-04-12 22:36:18 +0200

    -added completion route -added generate image route -generic error handlind

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,18 +1,86 @@
-const {OpenAIApi, Configuration} = require("azure-openai");
+const {OpenAIApi, Configuration} = require("openai");
 
 const configuration = new Configuration({
   apiKey: '',
-  azure: {
-    apiKey: '',
-    endpoint: 'https://hfgopenaieastus.openai.azure.com/',
-    deploymentName: 'GTPTurbo',
-  }
+  // azure: {
+  //   apiKey: '',
+  //   endpoint: 'https://hfgopenaieastus.openai.azure.com/',
+  //   deploymentName: 'GTPTurbo',
+  // }
 });
 const openai = new OpenAIApi(configuration);
 
 let chatMessages  = [[]]
 
+
+function errorHandler(error, res) {
+  if (error.response) {
+    console.error(error.response.status, error.response.data);
+    res.status(error.response.status).json(error.response.data);
+  } else {
+    console.error(`Error with OpenAI API request: ${error.message}`);
+    res.status(500).json({
+      error: {
+        message: 'An error occurred during your request.',
+      }
+    });
+  }
+}
+
+
 const appRouter = function (app) {
+
+
+  app.post("/complete", async function(req, res) {
+
+    let input= req.body.prompt;
+
+    console.log(input);
+
+    try {
+      const completion = await openai.createCompletion({
+        model:"text-davinci-003",
+        temperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        prompt: input,
+      });
+
+      console.log(completion.data.choices);
+
+      res.status(200).json(completion.data.choices);
+
+    } catch(error) {
+      errorHandler(error, res);
+    }
+  });
+
+  app.post("/image", async function(req, res) {
+
+    let input= req.body.prompt;
+
+    console.log("generating image for: "+ input);
+
+    try {
+      const completion = await openai.createImage({
+        prompt: input,
+        n: 1,
+        size: "1024x1024",
+      });
+      let image_url = completion.data.data[0].url;
+
+      console.log(completion.data);
+
+      res.status(200).json(image_url);
+
+    } catch(error) {
+      errorHandler(error, res);
+    }
+  });
+
+
   app.post("/", async function(req, res) {
 
     let id = req.body.text_1;
@@ -32,27 +100,15 @@ const appRouter = function (app) {
         temperature: 0.6,
       });
 
-      chatMessages[id].push({"role": completion.data.choices[0].message.role, "content": completion.data.choices[0].message.content})
 
       //Log conversation
       console.log(chatMessages[id]);
 
       res.status(200).json({ result: completion.data.choices[0].message});
+
+      chatMessages[id].push({"role": completion.data.choices[0].message.role, "content": completion.data.choices[0].message.content})
     } catch(error) {
-
-      if (error.response) {
-        console.error(error.response.status, error.response.data);
-        res.status(error.response.status).json(error.response.data);
-      } else {
-
-        console.error(`Error with OpenAI API request: ${error.message}`);
-
-        res.status(500).json({
-          error: {
-            message: 'An error occurred during your request.',
-          }
-        });
-      }
+      errorHandler(error, res);
     }
   });
 }
